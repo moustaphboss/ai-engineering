c0c64ef66cf57f8419a17e449fb7e667fe252dd1
Moustapha Kebe <mustafakebe99@gmail.com>
2026-08-09 18:12:25 +0200

    Remove electron-project, moved to standalone repo
    Now lives at github.com/moustaphboss/electron-agent with full commit
    history preserved via git subtree split.

--- electron-project/src/preload/preload.ts
+++
@@ -1,9 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-
-contextBridge.exposeInMainWorld("demoAPI", {
-  ping: () => ipcRenderer.invoke("ping"),
-  showSaveDialog: () => ipcRenderer.invoke("show-save-dialog"),
-  onMenuPing: (callback: () => void) => {
-    ipcRenderer.on("menu-ping", callback);
-  },
-});
